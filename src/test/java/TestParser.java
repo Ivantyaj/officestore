42f0809ca3eda4f3d1256385356830107038dee3
Ivantyaj <Ivantyaj@gmail.com>
2018-11-27 22:14:55 +0300

    test bu work

---
+++ src/test/java/TestParser.java
@@ -0,0 +1,32 @@
+
+import by.grouptopprogers.parsers.MyDOMparser;
+import org.junit.*;
+
+import java.util.ArrayList;
+
+public class TestParser {
+    //private static ArrayList<Category> categoryArrayList;
+    private static MyDOMparser domParser;
+
+    @BeforeClass
+    public static void initAction() {
+        domParser = MyDOMparser.getInstance();
+    }
+
+    @AfterClass
+    public static void clearAction(){
+        domParser = null;
+    }
+
+    @Test(timeout=10)
+    public void testParse(){
+        Assert.assertNotNull(MyDOMparser.getInstance().parseFile());
+    }
+
+    @Ignore("not ready yet")
+    @Test
+    public void testDataParse(){
+        Assert.assertEquals(categoryArrayList, MyDOMparser.getInstance().parseFile());
+    }
+
+}
