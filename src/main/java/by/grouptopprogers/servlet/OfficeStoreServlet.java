802b37919b6020df1327e3f40accfad6fea87bdd
Vladislav <vladstepovoyvios@gmail.com>
2018-11-27 22:08:54 +0300

    add to index.jsp ,removed from servlet

--- src/main/java/by/grouptopprogers/servlet/OfficeStoreServlet.java
+++ src/main/java/by/grouptopprogers/servlet/OfficeStoreServlet.java
@@ -1,67 +1,81 @@
 package by.grouptopprogers.servlet;
 
 import by.grouptopprogers.entity.Category;
-import by.grouptopprogers.entity.Product;
 import by.grouptopprogers.parsers.MyDOMparser;
 import org.xml.sax.SAXException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+
+
 public class OfficeStoreServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter printWriter = response.getWriter();
-        List<Category> categoryList = null;
+
+    List<Category> categoryList = null;
+
+    public void init() throws ServletException {
         try {
             categoryList = MyDOMparser.getInstance().parseFile();
-        } catch (SAXException | ParserConfigurationException e) {
+        } catch (SAXException | ParserConfigurationException | IOException e) {
             e.printStackTrace();
         }
+    }
 
-        printWriter.println("<p align=\"center\">");
-        printWriter.println("PRODUCTS");
-        printWriter.println("</span>");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       // response.setContentType("text/html");
+       // response.setCharacterEncoding("UTF-8");
+       // PrintWriter printWriter = response.getWriter();
+//        List<Category> categoryList = null;
+//        try {
+//            categoryList = MyDOMparser.getInstance().parseFile();
+//        } catch (SAXException | ParserConfigurationException e) {
+//            e.printStackTrace();
+//        }
+        request.setAttribute("categoryList", categoryList);
+        this.getServletContext().getRequestDispatcher("WEB-INF/index.jsp").forward(request,response);
 
-        try {
-
-            for (Category category : categoryList) {
-                printWriter.println("<table border=1 align=\"center\">");
-                printWriter.println("<caption>" +
-                        category.getNameCategory() +
-                        "</caption>");
-                printWriter.println("<tr>" +
-                        //"<th>Category name</th>" +
-                        "<th>ID</th>" +
-                        "<th>Type</th>" +
-                        "<th>Company</th>" +
-                        "<th>Purpose</th>" +
-                        "<th>Currency</th>" +
-                        "<th>Value</th>" +
-                        "</tr>");
-                printWriter.println("<tr>");
-                //printWriter.println("<td>" + category.getNameCategory() + "</td>");
-                for (Product product : category.getListProducts()) {
-                    printWriter.println("<tr><td>" + product.getId() + "</td>");
-                    printWriter.println("<td>" + product.getType() + "</td>");
-                    printWriter.println("<td>" + product.getCompany() + "</td>");
-                    printWriter.println("<td>" + product.getPurpose() + "</td>");
-                    printWriter.println("<td>" + product.getCost().getCurrency() + "</td>");
-                    printWriter.println("<td>" + product.getCost().getValue() + "</td></tr>");
-                }
-                printWriter.println("</tr>");
-                printWriter.println("</table>");
-            }
-        } catch (Exception e) {
-            throw new ServletException(e);
-        }
+//                request.getSergetCgetRequestDispatcher("WEB-INF/index.jsp");
+//        rd.forward(request, response);
+//        printWriter.println("<p align=\"center\">");
+//        printWriter.println("PRODUCTS");
+//        printWriter.println("</span>");
+//
+//        try {
+//            for (Category category : categoryList) {
+//                printWriter.println("<table border=1 align=\"center\">");
+//                printWriter.println("<caption>" +
+//                        category.getNameCategory() +
+//                        "</caption>");
+//                printWriter.println("<tr>" +
+//                        "<th>ID</th>" +
+//                        "<th>Type</th>" +
+//                        "<th>Company</th>" +
+//                        "<th>Purpose</th>" +
+//                        "<th>Currency</th>" +
+//                        "<th>Value</th>" +
+//                        "</tr>");
+//                printWriter.println("<tr>");
+//                for (Product product : category.getListProducts()) {
+//                    printWriter.println("<tr><td>" + product.getId() + "</td>");
+//                    printWriter.println("<td>" + product.getType() + "</td>");
+//                    printWriter.println("<td>" + product.getCompany() + "</td>");
+//                    printWriter.println("<td>" + product.getPurpose() + "</td>");
+//                    printWriter.println("<td>" + product.getCost().getCurrency() + "</td>");
+//                    printWriter.println("<td>" + product.getCost().getValue() + "</td></tr>");
+//                }
+//                printWriter.println("</tr>");
+//                printWriter.println("</table>");
+//            }
+//        } catch (Exception e) {
+//            throw new ServletException(e);
+//        }
 
 
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,25 @@
-<%@ page language="java" contentType="text/html; charset=utf-8"  pageEncoding="ISO-8859-5"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="ISO-8859-5" %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional //EN">
-<html><head>
+<html>
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Info about Request </title>
-</head><body>
-<FORM action="store" method="GET">
-    <INPUT type="submit"  value="Выполнить">
-</FORM>
-</body></html>
+</head>
+<body>
+
+
+    <p>
+        <%if (request.getAttribute("categoryList") != null) {
+            out.println("<p>" + request.getAttribute("my_class").toString() + "</p>");
+        }%>
+    </p>
+    <%--<c:forEach var="item" items="${categoryList}">--%>
+        <%--<tr>--%>
+            <%--<c:forEach var="column" items="${item}">--%>
+                <%--<td>${column}</td>--%>
+            <%--</c:forEach>--%>
+        <%--</tr>--%>
+    <%--</c:forEach>--%>
+</body>
+</html>
